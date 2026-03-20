8cf8d66be2bfb71521ae67955f6c8cb48aa7c5c8
Rishith67 <rishithreddy69420@gmail.com>
2026-03-20 13:17:31 +0530

    'trigger'

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ceerplanner/', // GitHub Pages base: https://ceeplannner.github.io/ceeplanner/
+  base: '/ceerplanner/', // GitHub Pages base: https://ceerplanner.github.io/ceerplanner/
 })
